4489be60f04dbe0358084d78c68145bf14b35d14
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-05 18:40:38 +0530

    XAMARIN-35082 - Sample and screenshot added.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,50 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        ToolbarItem day, week;
+        DateTime? minDate, maxDate;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            schedule = bindable.FindByName<SfSchedule>("schedule");
+            day = bindable.FindByName<ToolbarItem>("Day");
+            week = bindable.FindByName<ToolbarItem>("Week");
+
+            schedule.VisibleDatesChangedEvent += Schedule_VisibleDatesChangedEvent;
+
+            day.Clicked += Day_Clicked;
+            week.Clicked += Week_Clicked;
+        }
+        private void Schedule_VisibleDatesChangedEvent(object sender, VisibleDatesChangedEventArgs e)
+        {
+            if (schedule.ScheduleView == ScheduleView.WeekView)
+            {
+                minDate = e.visibleDates[0].Date;
+                maxDate = e.visibleDates[e.visibleDates.Count - 1].Date;
+
+                //// We have already used DateTime of MinValue and MaxValue as threshold value
+                //// in Source , so that set hardcoded values
+                schedule.MinDisplayDate = new DateTime(01, 01, 01);
+                schedule.MaxDisplayDate = new DateTime(9999, 12, 31);
+            }
+        }
+        private void Week_Clicked(object sender, EventArgs e)
+        {
+            schedule.ScheduleView = ScheduleView.WeekView;
+        }
+        private void Day_Clicked(object sender, EventArgs e)
+        {
+            schedule.ScheduleView = ScheduleView.DayView;
+            schedule.MinDisplayDate = (DateTime)minDate;
+            schedule.MaxDisplayDate = (DateTime)maxDate;
+        }
+    }
+}
---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,142 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// current day meetings 
+        /// </summary>
+        private List<string> currentDayMeetings;
+
+        /// <summary>
+        /// list of meeting
+        /// </summary>
+        private ScheduleAppointmentCollection appointments;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppointmentEditorViewModel" /> class.
+        /// </summary>
+        public SchedulerViewModel()
+        {
+            this.Appointments = new ScheduleAppointmentCollection();
+            this.InitializeDataForBookings();
+            this.BookingAppointments();
+        }
+
+        /// <summary>
+        /// Property changed event handler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets list of meeting
+        /// </summary>
+        public ScheduleAppointmentCollection Appointments
+        {
+            get
+            {
+                return this.appointments;
+            }
+
+            set
+            {
+                this.appointments = value;
+                this.RaiseOnPropertyChanged("Appointments");
+            }
+        }
+        /// <summary>
+        /// Method for booking appointments.
+        /// </summary>
+        private void BookingAppointments()
+        {
+            Random randomTime = new Random();
+            List<Point> randomTimeCollection = this.GettingTimeRanges();
+
+            DateTime date;
+            DateTime dateFrom = DateTime.Now.AddDays(-10);
+            DateTime dateTo = DateTime.Now.AddDays(10);
+            DateTime dateRangeStart = DateTime.Now.AddDays(-3);
+            DateTime dateRangeEnd = DateTime.Now.AddDays(3);
+
+            for (date = dateFrom; date < dateTo; date = date.AddDays(1))
+            {
+                if ((DateTime.Compare(date, dateRangeStart) > 0) && (DateTime.Compare(date, dateRangeEnd) < 0))
+                {
+                    for (int additionalAppointmentIndex = 0; additionalAppointmentIndex < 3; additionalAppointmentIndex++)
+                    {
+                        ScheduleAppointment meeting = new ScheduleAppointment();
+                        int hour = randomTime.Next((int)randomTimeCollection[additionalAppointmentIndex].X, (int)randomTimeCollection[additionalAppointmentIndex].Y);
+                        meeting.StartTime = new DateTime(date.Year, date.Month, date.Day, hour, 0, 0);
+                        meeting.EndTime = meeting.StartTime.AddHours(1);
+                        meeting.Subject = this.currentDayMeetings[randomTime.Next(9)];
+                        meeting.Color = Color.FromHex("#889e81");
+                        meeting.IsAllDay = false;
+                        meeting.StartTimeZone = string.Empty;
+                        meeting.EndTimeZone = string.Empty;
+                        this.Appointments.Add(meeting);
+                    }
+                }
+                else
+                {
+                    ScheduleAppointment meeting = new ScheduleAppointment();
+                    meeting.StartTime = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(9, 11), 0, 0);
+                    meeting.EndTime = meeting.StartTime.AddDays(2).AddHours(1);
+                    meeting.Subject = this.currentDayMeetings[randomTime.Next(9)];
+                    meeting.Color = Color.FromHex("#889e81");
+                    meeting.IsAllDay = true;
+                    meeting.StartTimeZone = string.Empty;
+                    meeting.EndTimeZone = string.Empty;
+                    this.Appointments.Add(meeting);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method for get timing range.
+        /// </summary>
+        /// <returns>return time collection</returns>
+        private List<Point> GettingTimeRanges()
+        {
+            List<Point> randomTimeCollection = new List<Point>();
+            randomTimeCollection.Add(new Point(9, 11));
+            randomTimeCollection.Add(new Point(12, 14));
+            randomTimeCollection.Add(new Point(15, 17));
+
+            return randomTimeCollection;
+        }
+
+
+        /// <summary>
+        /// Method for initialize data bookings.
+        /// </summary>
+        private void InitializeDataForBookings()
+        {
+            this.currentDayMeetings = new List<string>();
+            this.currentDayMeetings.Add("General Meeting");
+            this.currentDayMeetings.Add("Plan Execution");
+            this.currentDayMeetings.Add("Project Plan");
+            this.currentDayMeetings.Add("Consulting");
+            this.currentDayMeetings.Add("Performance Check");
+            this.currentDayMeetings.Add("Yoga Therapy");
+            this.currentDayMeetings.Add("Plan Execution");
+            this.currentDayMeetings.Add("Project Plan");
+            this.currentDayMeetings.Add("Consulting");
+            this.currentDayMeetings.Add("Performance Check");
+        }
+        /// <summary>
+        /// Invoke method when property changed
+        /// </summary>
+        /// <param name="propertyName">property name</param>
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
